d0e04a03e57f2e0d86a39119d4aec999e55311b1
APRILT24 <aprilt24@Mac.fritz.box>
2026-03-09 11:50:57 +0100

    Added open sans as project font

--- mining-database/index.php
+++ mining-database/index.php
@@ -29,6 +29,11 @@ $dataExists = file_exists(__DIR__ . '/data/processed/all_stakeholders.json');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nigeria Mining Stakeholder Database</title>
     
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    
     <!-- Styles -->
     <link rel="stylesheet" href="assets/css/mining-db.css">
     
